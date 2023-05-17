8906e67edd3132557b7ad5fad5c3c1978f9344c9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-17 20:13:51 -0300

    Site com java e html

--- cp2.js
+++ cp2.js
@@ -1,76 +1,128 @@
 const prinome = document.querySelector('#prinome');
 const erro = document.querySelector('#erro');
+
 const secnome = document.querySelector('#secnome');
 const erro2 = document.querySelector('#erro2');
+
 const email = document.querySelector('#email');
 const erro3 = document.querySelector('#erro3');
+
 const senha = document.querySelector('#senha');
 const erro4 = document.querySelector('#erro4');
+
 const checksenha = document.querySelector('#checksenha');
 const erro5 = document.querySelector('#erro5');
 
+const lblprinome = document.getElementById('lblprinome');
+const lblsecnome = document.getElementById('lblsecnome');
+const lblemail = document.getElementById('lblemail');
+const lblsenha = document.getElementById('lblsenha');
+const lblcheck = document.getElementById('lblcheck');
 
+// |----------------------------------------------------------------------------------------------------------------|
 
-prinome.addEventListener('focus', () => {
+prinome.addEventListener('input', () => {
+  if (prinome.value.trim() === '' || prinome.value.length < 5) {
+    lblprinome.style.color = 'red';
+  } else {
+    lblprinome.style.color = 'green';
+  }
+});
+
+prinome.addEventListener('blur', () => {
+  if (prinome.value.trim() === '' || prinome.value.length < 5) {
+    erro.textContent = 'O campo deve ter pelo menos 5 caracteres e não pode ficar vazio.';
+  } else {
     erro.textContent = '';
-    prinome.removeAttribute('style');
-  });
-  
-  prinome.addEventListener('blur', () => {
-    if (prinome.value.trim() === '' || prinome.value.length < 5) {
-      erro.textContent = 'O campo deve ter pelo menos 5 caracteres e não pode ficar vazio.';
+  }
+});
+
+// |----------------------------------------------------------------------------------------------------------------|
+ 
+  secnome.addEventListener('input', () => {
+    if (secnome.value.trim() === '' || secnome.value.length < 5) {
+      lblsecnome.style.color = 'red';
+    } else {
+      lblsecnome.style.color = 'green';
     }
-  });
-
-
-
-  secnome.addEventListener('focus', () => {
-    erro2.textContent = '';
   });
   
   secnome.addEventListener('blur', () => {
     if (secnome.value.trim() === '' || secnome.value.length < 5) {
       erro2.textContent = 'O campo deve ter pelo menos 5 caracteres e não pode ficar vazio.';
+    } else {
+      erro2.textContent = '';
     }
   });
 
 
+// |----------------------------------------------------------------------------------------------------------------|
 
-  email.addEventListener('focus', () => {
-    erro3.textContent = '';
-  })
+
+  email.addEventListener('input', () => {
+    if (email.value.trim() === '' || email.value.length < 5 || !email.value.includes('@')) {
+      lblemail.style.color = 'red';
+    } else {
+      lblemail.style.color = 'green';
+    }
+  });
+  
   email.addEventListener('blur', () => {
-    if (email.value.trim() === '' || email.value.lenght < 5 || !email.value.includes('@')){
-        erro3.textContent = 'O campo deve ter pelo menos 5 caracteres, não pode ficar vazio e tem que contar o caractere"@" para ser validado como um email'
+    if (email.value.trim() === '' || email.value.length < 5 || !email.value.includes('@')) {
+      erro3.textContent = 'O campo deve ter pelo menos 5 caracteres e não pode ficar vazio.';
+    } else {
+      erro3.textContent = '';
     }
-  })
-
-  
-  senha.addEventListener('focus', () => {
-    erro4.textContent = '';
   });
+
+
+
+  // |----------------------------------------------------------------------------------------------------------------|
+  
+
+
+
+  senha.addEventListener('input', () => {
+    if (senha.value.trim() === '' || senha.value.length < 6 || senha.value.length > 8) {
+      lblsenha.style.color = 'red';
+    } else {
+      lblsenha.style.color = 'green';
+    }
+  });
+  
   senha.addEventListener('blur', () => {
-    if (senha.value.trim() === '') {
-      erro4.textContent = 'A senha não pode ficar vazia.';
-    } else if (senha.value.length < 6 || senha.value.length > 8) {
-      erro4.textContent = 'A senha deve ter entre 6 e 8 caracteres.';
+    if (senha.value.trim() === '' || senha.value.length < 6 || senha.value.length > 8 ) {
+      erro4.textContent = 'O campo deve ter pelo menos 5 caracteres e não pode ficar vazio.';
+    } else {
+      erro4.textContent = '';
     }
   });
 
+
+
+
   
-  
-  checksenha.addEventListener('focus', () => {
-    erro5.textContent = '';
+  // |----------------------------------------------------------------------------------------------------------------|
+
+
+  checksenha.addEventListener('input', () => {
+    if (senha.value !== checksenha.value) {
+      lblcheck.style.color = 'red';
+    } else {
+      lblcheck.style.color = 'green';
+    }
   });
+  
   checksenha.addEventListener('blur', () => {
     if (senha.value !== checksenha.value) {
-        erro5.textContent = 'As senhas precisam ser iguais para ser cadastradas'
+      erro5.textContent = 'As senhas não coincidem.';
+    } else {
+      erro5.textContent = '';
     }
   });
 
 
-
-
+// |----------------------------------------------------------------------------------------------------------------|
 
 
 let darkMode = false;
